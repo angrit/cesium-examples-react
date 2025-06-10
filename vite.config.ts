bc444dd057366d144ff2f73098ad4c2c6466c945
Richard Mudway <richmudway@gmail.com>
2025-06-10 17:38:02 +0700

    Add new package and config use in project

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import cesium from 'vite-plugin-cesium'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), cesium()],
+  base: '/cesium-examples-react',
   build: {
     commonjsOptions: {
       // Ensure CommonJs modules are properly handled
